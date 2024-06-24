ccec88b363a1004baecf415fdada201d3568f284
asafe <asafe.kauffmann@llam.tec.br>
2024-06-24 00:16:51 -0300

    Create last waves

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css'
 import MyHero from './components/MyHero'
 import Documentation from './components/Documentation'
 import secondWave from './assets/images/second-wave.svg'
+import fourWave from './assets/images/four-wave.svg'
+import threeWave from './assets/images/three-wave.svg'
 
 
 function App() {
@@ -66,10 +68,8 @@ function App() {
         <img src={secondWave} className="w-full h-auto" />
       </div> 
 
-
-
-      <div className="card bg-lighter-blue">
-        <h1 className="text-medium-blue text-5xl">
+      <div className="card bg-lighter-blue h-[80vh]">
+        <h1 className="text-medium-blue text-4xl">
           Welcome to Design Map
         </h1>
         <p className="text-paragh-gray">
@@ -80,27 +80,50 @@ function App() {
         </button>
       </div>
       
-      <div className="card bg-light-yellow">
-        <h1 className="text-dark-blue text-5xl">
-          Welcome to Design Map
-        </h1>
-        <p className="text-paragh-gray">
-          Click on the Vite and React logos to learn more
-        </p>
-        <p className="text-dark-gray">
-          Click on the Vite and React logos to learn more
-        </p>
-        <button className="bg-light-blue text-white" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      {/* Background Wave */}
+      <div className="w-full mt-0 bg-lighter-blue">
+          <img src={threeWave} className="w-full" />
+        </div> 
 
+      <div className="w-full flex flex-col items-center justify-center bg-dark-blue"> 
+        <div className="card ">
+          <h1 className="text-white text-5xl">
+            Welcome to Design Map
+          </h1>
+          <p className="text-paragh-gray">
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+            Click on the Vite and React logos to learn more
+          </p>
+          <p className="text-dark-gray">
+            Click on the Vite and React logos to learn more
+          </p>
+          <button className="bg-light-blue text-white" onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+
+        {/* Background Wave */}
+        <div className="w-full mb-0 bg-lighter-blue">
+          <img src={fourWave} className="w-full" />
+        </div> 
+      </div>
+      
       <div className="bg-dark-gray flex justify-center content-center" >
         <a href="https://www.figma.com/design/dDzuNWi0mSH6oIqU11mMDc/Background-TestModel?node-id=0-1&t=1hiQ5kDFhrNPc9xa-0" target="_blank">
           <img src={logoDoSomeThing} className="logo" alt="Do Some Thing logo" />
         </a>
-      </div>
-
+      </div> 
+      
     </>
   )
 }
